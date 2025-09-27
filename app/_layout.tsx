b85ce83450ab3a23dd63c2189bad12f92391bc7e
wolfag <wolfag91@gmail.com>
2025-09-27 15:50:47 +0700

    chore(init): config fake server + react query

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,12 +1,15 @@
+import { queryClient } from '@/lib/queryClient';
+import { QueryClientProvider } from '@tanstack/react-query';
 import { Stack } from 'expo-router';
 import 'react-native-reanimated';
 
+
 export default function RootLayout() {
   return (
-    <>
+    <QueryClientProvider client={queryClient}>
       <Stack>
         <Stack.Screen name='index' options={{ headerShown: false }} />
       </Stack>
-    </>
+    </QueryClientProvider>
   );
 }
--- app/index.tsx
+++ app/index.tsx
@@ -1,7 +1,18 @@
+import { useQuery } from '@tanstack/react-query';
 import React from 'react';
 import { Text, View } from 'react-native';
 
 const Home = () => {
+  const { data } = useQuery({
+    queryKey: ['test'],
+    queryFn: async () => {
+      const res = await fetch('/api/currencies');
+      const json = await res.json();
+      return json;
+    },
+  });
+  console.log({ data });
+
   return (
     <View
       style={{
