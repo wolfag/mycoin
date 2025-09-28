c3e4fc964c368108e622d4c56a5b9781ce8a99f2
wolfag <wolfag91@gmail.com>
2025-09-28 11:07:41 +0700

    chore(cleanup): remove react-query and server config

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,18 +1,15 @@
-import { queryClient } from '@/lib/queryClient';
-import { QueryClientProvider } from '@tanstack/react-query';
 import { Stack } from 'expo-router';
 import 'react-native-reanimated';
 import Toast from 'react-native-toast-message';
 
-
 export default function RootLayout() {
   return (
-    <QueryClientProvider client={queryClient}>
+    <>
       <Stack>
         <Stack.Screen name='index' options={{ headerShown: false }} />
         <Stack.Screen name='currency-list' options={{ headerShown: false }} />
       </Stack>
       <Toast />
-    </QueryClientProvider>
+    </>
   );
 }
